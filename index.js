37eff5663994be30add6ac3795d4194d8e92efb3
eugmukhin <eugmukhin@yandex.ru>
2018-08-21 12:40:35 +0300

    Fix. Add env defaults

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const http = require('http');
 const port = 3000;
-const LOG_INTERVAL = parseInt(process.env.LOG_INTERVAL);
-const LOG_STOP = parseInt(process.env.LOG_STOP);
+const LOG_INTERVAL = parseInt(process.env.LOG_INTERVAL) || 1000;
+const LOG_STOP = parseInt(process.env.LOG_STOP) || 10000;
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url !== '/favicon.ico') {
